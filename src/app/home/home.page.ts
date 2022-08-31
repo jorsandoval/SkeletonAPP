361422da39721c7f7ea45c29dfdd01c0fc91eb9f
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-08-31 00:10:23 -0400

    avance

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AnimationController } from '@ionic/angular';
+import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,21 +8,27 @@ import { AnimationController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private usuario: string = this.router.getCurrentNavigation().extras.state.user;
 
-  constructor(private animationCtrl: AnimationController) {
-
+  constructor(private animationCtrl: AnimationController
+    , private activeroute: ActivatedRoute,
+    private router: Router) {
+    this.activeroute.queryParams.subscribe(params => {
+      console.log(this.router.getCurrentNavigation());
+    });
   }
 
-  ngOnInit(){}
+  ngOnInit() {
+  }
   ngAfterViewInit() {
     const animation =
       this.animationCtrl.create()
         .addElement(document.querySelector('.pac-man'))
         .duration(2500)
         .iterations(20)
-        .fromTo('transform','translateX(0px)','translateX(800px)')
-        .fromTo('opacity','1','0');
-      animation.play();
+        .fromTo('transform', 'translateX(0px)', 'translateX(800px)')
+        .fromTo('opacity', '1', '0');
+    animation.play();
   }
 
 
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationExtras } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  private usuario: string = '';
+  constructor(private router: Router) {
+  }
+  ngOnInit(): void {
 
   }
 
+  ngOnclick() {
+    let navigationExtras: NavigationExtras = {
+      state: {user: this.usuario}
+    };
+    this.router.navigate(['/home'], navigationExtras);
+  }
 }
